5fc7ef91aadda590bf0ab280c88471e8fef8955a
serkizka <anastasiaserkiz@gmail.com>
2022-08-09 17:56:05 +0300

    09.08 filmstrip-viewport

---
+++ app/design/frontend/Pixelpro/ThemePro/web/js/sections/materials.js
@@ -0,0 +1,29 @@
+define ([
+  'jquery',
+  'slick'
+], function ($) {
+  let materialsSlider = $('.materials > .pagebuilder-column-group');
+
+  if (materialsSlider.length) {
+
+    materialsSlider.slick({
+      slidesToScroll: 1,
+      swipeToSlide: true,
+      slidesToShow: 8,
+      dots: false,
+      infinite: false,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            centerPadding: '60px',
+            centerMode: true,
+            arrows: false,
+            slidesToShow: 2,
+          }
+        }
+      ]
+    })
+  }
+})
