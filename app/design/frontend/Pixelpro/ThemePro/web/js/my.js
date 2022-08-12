936f00d595d79d0698d8137a1a29f3caa09c7276
serkizka <anastasiaserkiz@gmail.com>
2022-08-12 18:21:49 +0300

    12.08 popular-products

---
+++ app/design/frontend/Pixelpro/ThemePro/web/js/my.js
@@ -0,0 +1,7 @@
+const langTitle = document.getElementById("lang-title");
+
+
+langTitle.addEventListener("click", function () {
+ console.log('44444');
+   element.style.setProperty("text-decoration", "line-through");
+});
